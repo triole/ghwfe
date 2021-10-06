25829b68b841edce67415f0c0707c8e64105e5ea
olaf michaelis <o.mic@web.de>
2021-10-06 21:45:08 +0200

    Add ld flags to go build

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
+# Github Workflow Engine
 
+<!--- mdtoc: toc begin -->
+<!--- mdtoc: toc end -->
+
+Basically a collection of shell scripts and snippets used for making Github release builds.
--- sh/go_build.sh
+++ sh/go_build.sh
@@ -14,27 +14,25 @@ architectures=(
 )
 
 app_name=$(pwd | grep -Po "[^/]+$")
+ld_author=$(grep -Po "(?<=name\s=\s).*" ~/.gitconfig)
+ld_git_commit_no=$(git rev-list "origin/master" --count --)
+ld_git_commit_hash=$(git rev-parse HEAD)
+ld_date=$(LANG=en_us_88591 date)
 
-# ver=$(eval "${VERSION_COMMAND}")
-# echo ${ver}
-# if [[ -z "${ver}" ]]; then
-#     echo "Software version required. Please specify \$VERSION_COMMAND"
-#     exit 1
-# fi
+source_folder="${SOURCE_FOLDER}"
+target_folder="${TARGET_FOLDER}"
 
-source_folder="${1}"
-target_folder="${2}"
-if [[ -z "${url}" ]]; then
-    url="${URL}"
-fi
 if [[ -z "${source_folder}" ]]; then
     source_folder="${GITHUB_WORKSPACE}"
 fi
 if [[ -z "${target_folder}" ]]; then
-    target_folder="${TARGET_FOLDER}"
+    target_folder="build"
 fi
 
 gobin="${GO_BIN_PATH}"
+if [[ ! -f "${gobin}" ]]; then
+    gobin="$(which go)"
+fi
 
 debug="false"
 for val in "$@"; do
@@ -54,8 +52,17 @@ cd "${source_folder}"
 rcmd "${gobin} mod init ${app_name}"
 rcmd "${gobin} mod tidy"
 
+export CGO_ENABLED=0
 for arch in "${architectures[@]}"; do
     arch_name="$(echo "${arch}" | grep -Po ".*(?=:)")"
     arch="$(echo "${arch}" | grep -Po "[^:]+$")"
-    rcmd "${arch} CGO_ENABLED=0 ${gobin} build -a -o ${target_folder}/${arch_name}/${app_name} ."
+
+    rcmd "${gobin} build -o ${target_folder}/${arch_name}/${app_name} \
+        -ldflags \
+        \"-s -w -X 'main.BUILDTAGS={
+            _subversion: ${ld_git_commit_no}, author: ${ld_author},
+            build date: ${ld_date}, git hash: ${ld_git_commit_hash},
+        }'\" \
+        src/*.go"
+
 done
