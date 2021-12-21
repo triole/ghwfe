d107babb7f8b9c46e9ebef50d406131694009db5
olaf michaelis <o.mic@web.de>
2021-12-21 14:21:06 +0100

    Add sub package symlink script

---
+++ sh/go_add_sub_packages.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+IFS=$'\n'
+
+source_folder="${SOURCE_FOLDER}"
+if [[ -z "${source_folder}" ]]; then
+    source_folder="${GITHUB_WORKSPACE}"
+fi
+
+arr=($(find "${source_folder}" -mindepth 1 -maxdepth 1 -type d))
+
+for el in "${arr[@]}"; do
+    src="${el}"
+    pkg="$(echo "${el}" | grep -Po "^.*(?=/.*/)" | grep -Po "[^/]+$")"
+    subpkg="$(echo "${el}" | grep -Po "[^/]+$")"
+    trg="${GOROOT}/src/${pkg}/${subpkg}"
+
+    echo -e "Try to make symlink\n\t${src} -> ${trg}"
+    if [[ ! -f "${trg}" ]] && [[ ! -d "${trg}" ]] && [[ ! -L "${trg}" ]]; then
+        cmd="ln -s \"${src}\" \"${trg}\""
+        echo "${cmd}"
+    else
+        echo "Target exists. Can not make symlink at ${trg}"
+        exit 1
+    fi
+done
