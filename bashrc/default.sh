234ac02a14bd53553e132ba384d038fcec085a6f
olaf michaelis <o.mic@web.de>
2022-09-14 13:07:57 +0200

    Add default bashrc

---
+++ bashrc/default.sh
@@ -0,0 +1,27 @@
+alias ..="cd .."
+alias cl="clear"
+alias env="env | sort"
+alias gip="git pull"
+alias grep="grep --color"
+alias lsblk="lsblk -o name,maj:min,ro,rm,type,size,mountpoint,label,uuid,pttype,parttypename,type,vendor,model,serial"
+alias mic="micro"
+alias p="python"
+alias pm="python manage.py"
+alias pkl="pkill -9"
+alias psa='function _psa(){ if [[ -n "${1}" ]]; then ps faux | sift "${1}" | sift -v "sift.*${1}"; else ps faux; fi };_psa'
+alias spv="spv.sh"
+alias tailf="tail -F"
+alias tk="task"
+alias tlp="netstat -tulpen"
+alias tlps="sudo netstat -tulpen"
+
+if [[ -z $(which ls-go) ]]; then
+    export LS_COLORS=${LS_COLORS}:"di=1;34":"*.txt=1;36":"*.md=0;93"
+    alias l="ls --color=auto -CF"
+    alias ll="ls --color=auto -alF"
+    alias la="ls --color=auto -AlF"
+else
+    alias l="ls-go -n"
+    alias ll="ls-go -lnLS"
+    alias la="ls-go -lanLS"
+fi
