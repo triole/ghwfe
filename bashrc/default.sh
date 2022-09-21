27147cb94c446c6bea1e28a96985ac56be282752
olaf michaelis <o.mic@web.de>
2022-09-21 05:08:49 +0200

    Update default bashrc

--- bashrc/default.sh
+++ bashrc/default.sh
@@ -1,7 +1,6 @@
+export PS1="\u@\h \033[0;93m\w\033[0m \\033[1;92m\$\\033[0m "
 if [[ "$(whoami)" == "root" ]]; then
     export PS1="\u@\h \033[0;93m\w\033[0m \\033[1;31m# \\033[0m "
-else
-    export PS1="\u@\h \033[0;93m\w\033[0m \\033[1;92m\$\\033[0m "
 fi
 
 alias ..="cd .."
@@ -15,18 +14,17 @@ alias p="python"
 alias pm="python manage.py"
 alias pkl="pkill -9"
 alias psa='function _psa(){ if [[ -n "${1}" ]]; then ps faux | sift "${1}" | sift -v "sift.*${1}"; else ps faux; fi };_psa'
-alias spv="spv.sh"
 alias tailf="tail -F"
 alias tk="task"
 alias tlp="netstat -tulpen"
 alias tlps="sudo netstat -tulpen"
 
-if [[ -z $(which ls-go) ]]; then
-    export LS_COLORS=${LS_COLORS}:"di=1;34":"*.txt=1;36":"*.md=0;93"
-    alias l="ls --color=auto -CF"
-    alias ll="ls --color=auto -alF"
-    alias la="ls --color=auto -AlF"
-else
+export LS_COLORS=${LS_COLORS}:"di=1;34":"*.txt=1;36":"*.md=0;93"
+alias l="ls --color=auto -CF"
+alias ll="ls --color=auto -lF"
+alias la="ls --color=auto -AlF"
+
+if [[ -n $(which ls-go) ]]; then
     alias l="ls-go -n"
     alias ll="ls-go -lnLS"
     alias la="ls-go -lanLS"
