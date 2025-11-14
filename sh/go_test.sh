809911e47e6e960d38ab3f5bb9b293eb5b146f65
olaf michaelis <o.mic@web.de>
2025-11-14 08:06:34 +0100

    Add cd to source

--- sh/go_test.sh
+++ sh/go_test.sh
@@ -22,14 +22,13 @@ export GOROOT="${go_root}"
 
 source_folder="${SOURCE_FOLDER}"
 [[ -z "${source_folder}" ]] && source_folder="${GITHUB_WORKSPACE}"
-
 source_folder="$(realpath "${source_folder}")"
 
 target_folder="${TARGET_FOLDER}"
 [[ -z "${target_folder}" ]] && target_folder="/tmp/assets"
-
 target_folder="$(realpath "${target_folder}")"
 
 mkdir -p "${target_folder}"
 
-_rcmd go test -v -race -cover -bench=. ./...
+cd "${SOURCE_FOLDER}" &&
+  _rcmd go test -v -race -cover -bench=. ./...
