043d2b34d10c3091ad287c3051c8b9d72d1c9248
olaf michaelis <o.mic@web.de>
2025-11-14 08:14:56 +0100

    Fix paths

--- sh/go_test.sh
+++ sh/go_test.sh
@@ -16,19 +16,19 @@ _rcmd() {
 }
 
 go_root="/usr/local"
-[[ -n "${GOROOT}" ]] && go_root="${goroot}"
+[[ -n "${GOROOT}" ]] && go_root="${GOROOT}"
 
 export GOROOT="${go_root}"
 
-source_folder="${SOURCE_FOLDER}"
-[[ -z "${source_folder}" ]] && source_folder="${GITHUB_WORKSPACE}"
+source_folder="${GITHUB_WORKSPACE}"
+[[ -z "${source_folder}" ]] && source_folder="$(pwd)"
+[[ -n "${SOURCE_FOLDER}" ]] && source_folder="${SOURCE_FOLDER}"
 source_folder="$(realpath "${source_folder}")"
 
 target_folder="${TARGET_FOLDER}"
 [[ -z "${target_folder}" ]] && target_folder="/tmp/assets"
 target_folder="$(realpath "${target_folder}")"
 
-mkdir -p "${target_folder}"
-
-cd "${SOURCE_FOLDER}" &&
+echo mkdir -p "${target_folder}"
+cd "${source_folder}" &&
   _rcmd go test -v -race -cover -bench=. ./...
